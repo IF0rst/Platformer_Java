6510c841711b1362cb53470d8b893b55792cadc1
IF0rst <124600941+IF0rst@users.noreply.github.com>
2023-05-15 18:05:50 +0200

    Delete Engine directory

--- Engine/InstanceInterface.java
+++
@@ -1,9 +0,0 @@
-package Engine;
-
-import Instances.Instance;
-
-public interface InstanceInterface {
-    public void CalculatePhysics(Instance Selected);
-    public Instance IsTouching(Instance Selected);
-    public GameData getGameData();
-}
